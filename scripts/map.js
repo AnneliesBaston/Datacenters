ace6e0497be968527d28a39b4ae33dd84bca24bc
AnneliesBaston <44698893+AnneliesBaston@users.noreply.github.com>
2018-12-28 12:07:13 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -129,9 +129,10 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup('<b>' + point['Name'] + '</b><br>' +
          // (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +//
-          ('<tr><td><b>Products 2014: </b></td>'+ '<td>€  ' + point['Products_14'] + '</td></tr>') +
+          ('<table style="width:100%">') +
+	        ('<tr><td><b>Products 2014: </b></td>'+ '<td>€  ' + point['Products_14'] + '</td></tr>') +
           ('<tr><td><b>Projects 2014: </b></td>'+ '<td>€  ' + point['Projects_14'] + '</td></tr>') +
-          ('<tr><td><b>Sourcing 2014: </b></td>'+ '<td>€  ' + point['Sourcing_14'] + '</td></tr>') +
+          ('<tr><td><b>Sourcing 2014: </b></td>'+ '<td>€  ' + point['Sourcing_14'] + '</td></tr>') + 
           ('<tr><td><b>Products 2015: </b></td>'+ '<td>€  ' + point['Products_15'] + '</td></tr>') +
           ('<tr><td><b>Projects 2015: </b></td>'+ '<td>€  ' + point['Projects_15'] + '</td></tr>') +
           ('<tr><td><b>Sourcing 2015: </b></td>'+ '<td>€  ' + point['Sourcing_15'] + '</td></tr>') +
@@ -142,7 +143,8 @@ $(window).on('load', function() {
           ('<tr><td><b>Sourcing 2017: </b></td>'+ '<td>€  ' + point['Sourcing_17'] + '</td></tr>') +
           ('<tr><td><b>Products 2018: </b></td>'+ '<td>€  ' + point['Products_18'] + '</td></tr>') +
           ('<tr><td><b>Projects 2018: </b></td>'+ '<td>€  ' + point['Projects_18'] + '</td></tr>') +
-          ('<tr><td><b>Sourcing 2018: </b></td>'+ '<td>€  ' + point['Sourcing_18'] + '</td></tr>'));
+          ('<tr><td><b>Sourcing 2018: </b></td>'+ '<td>€  ' + point['Sourcing_18'] + '</td></tr>') +
+	        ('</table>'));
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
